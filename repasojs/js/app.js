4a3f40b2fc7ee082071901d4c28c40837768e505
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 13:58:49 -0700

    Object destructuring

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,38 +1,25 @@
-// Objetos
+// Destructuring de objetos
 
-// function mostrarInformacionTarea(tarea, prioridad) {
-//     return `La tarea ${tarea} tiene una prioridad de ${prioridad}`;
-// }
-
-// Object literal
-const persona = {
-    nombre: 'Juan',
-    profesion: 'Web Dev',
-    edad: 22,
-    isAdult: true
+const aprendiendoJS = {
+    version: {
+        nueva: 'ES6+',
+        anterior: 'ES5'
+    },
+    frameworks : [ 
+        'React', 'VueJS', 'AngularJS'
+    ]
 }
 
-// console.log(persona)
+// Destructuring es extraer valores de un objeto
+// console.log(aprendiendoJS);
 
-// Mala forma de usar la función
-// const mostrarCliente = mostrarInformacionTarea(persona.nombre, persona.profesion);
-// console.log(mostrarCliente);
+// version anterior
+// let version = aprendiendoJS.version.nueva;
+// console.log(version);
 
-// Object constructor
-function Tarea(nombre, urgencia) {
-    this.nombre = nombre;
-    this.urgencia = urgencia;
-}
+// Destrcuturing forma nueva
+let{version} = aprendiendoJS;
+console.log(version);
 
-// Agregar un prototype a tarea: 
-Tarea.prototype.mostrarInformacionTarea = function() {
-    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
-}
-
-// Crear una nueva tarea
-const tarea1 = new Tarea('Aprender Javascript y React', 'Urgente');
-console.log(tarea1);
-console.log(tarea1.mostrarInformacionTarea());
-
-// const mostrarInfo = mostrarInformacionTarea(tarea1.nombre, tarea1.urgencia);
-// console.log(mostrarInfo);
+let{nueva} = aprendiendoJS.version;
+console.log(nueva);
