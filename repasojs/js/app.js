045015ebd7261564b86b9b595dfe66e073ebec3d
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 14:22:25 -0700

    Arreglos, maps y Objects.keys

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,14 +1,36 @@
-// Métodos o funciones en un objeto
+// Arreglos y .map
 
+const carrito = ['Produco 1', 'Produco 2', 'Produco 3'];
+
+console.log(carrito); // Array(3) [ "Produco 1", "Produco 2", "Produco 3" ]
+
+const appContenedor = document.querySelector('#app');
+appContenedor.innerHTML = carrito;
+
+let html = '';
+
+carrito.forEach(producto => {
+    html+= `<li>${producto}</li>`;
+})
+appContenedor.innerHTML = html;
+
+// MAP
+carrito.map(producto => {
+    return 'Elproducto es' + producto;
+})
+// Output:
+// 0: "Elproducto esProduco 1"
+// 1: "Elproducto esProduco 2"
+// 2: "Elproducto esProduco 3"
+// length: 3
+
+
+// Object keys
 const persona = {
     nombre: 'Juan',
-    trabajo: 'Web Dev',
-    edad: 22,
-    musicIsRock: true,
-    mostrarInformacion: function() {
-        console.log(`${this.nombre} es ${this.trabajo} y su edad es
-        ${this.edad}`)
-    }
+    profesion: 'Web Dev',
+    edad: 22
 }
 
-persona.mostrarInformacion();
+console.log(persona); // Object { nombre: "Juan", profesion: "Web Dev", edad: 22 }
+console.log(Object.keys(persona)); //Array(3) [ "nombre", "profesion", "edad" ]
