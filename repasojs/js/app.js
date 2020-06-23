bba316958f0829c6b78fa8645b44113a12c549d0
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 10:48:49 -0700

    Prototypes

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,5 +1,9 @@
 // Objetos
 
+// function mostrarInformacionTarea(tarea, prioridad) {
+//     return `La tarea ${tarea} tiene una prioridad de ${prioridad}`;
+// }
+
 // Object literal
 const persona = {
     nombre: 'Juan',
@@ -8,13 +12,27 @@ const persona = {
     isAdult: true
 }
 
+// console.log(persona)
+
+// Mala forma de usar la función
+// const mostrarCliente = mostrarInformacionTarea(persona.nombre, persona.profesion);
+// console.log(mostrarCliente);
+
 // Object constructor
 function Tarea(nombre, urgencia) {
     this.nombre = nombre;
     this.urgencia = urgencia;
 }
 
+// Agregar un prototype a tarea: 
+Tarea.prototype.mostrarInformacionTarea = function() {
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`;
+}
+
 // Crear una nueva tarea
 const tarea1 = new Tarea('Aprender Javascript y React', 'Urgente');
 console.log(tarea1);
-console.log(tarea1.nombre);
+console.log(tarea1.mostrarInformacionTarea());
+
+// const mostrarInfo = mostrarInformacionTarea(tarea1.nombre, tarea1.urgencia);
+// console.log(mostrarInfo);
