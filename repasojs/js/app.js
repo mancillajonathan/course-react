d33550a89ae23af42079fa8a1c700d7d3c1f0744
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 18:57:23 -0700

    Programación Orientada a Objetos - Clases

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,51 +1,24 @@
-// Mostrando el resultado en HTML
-const descargarUsuarios = cantidad => new Promise((resolve, reject) => {
-    // Pasar la cantidad a la api
-    const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
+// Programación Orientada a Objetos - Clases
 
-    // Llamado a ajax
-    const xhr = new XMLHttpRequest();
-
-    // Abrir la conexión
-    xhr.open('GET', api, true);
-
-    // On load
-    xhr.onload = () => {
-        if(xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText).results);
-        } else {
-            reject(Error(xhr.statusText));
-        }
+class Tarea {
+    constructor(nombre, prioridad) {
+        this.nombre = nombre;
+        this.prioridad = prioridad;
     }
 
-    // Opctional: on error
-    xhr.onerror = (error) => reject(error);
-
-    // Send
-    xhr.send()
-});
-
-descargarUsuarios(10)
-    .then(
-      miembros => imprimirHTML(miembros),
-      error => console.error(
-          new Error('Hubo un error' + error)
-      )
-    );
-
-function imprimirHTML(usuarios) {
-    let html = '';
-    usuarios.forEach(usuario => {
-        html += `
-        <li>
-            Nombre: ${usuario.name.first} ${usuario.name.last}
-            País: ${usuario.nat}
-            Imágen:
-                <img src="${usuario.picture.medium}"> 
-        </li>
-        `
-    });
-
-    const contenedorApp = document.querySelector('#app');
-    contenedorApp.innerHTML = html;
+    // Método
+    mostrar() {
+        // console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
+        return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
+    }
 }
+
+// Crear objetos
+let tarea1 = new Tarea('Aprender Javascript', 'Alta');
+let tarea2 = new Tarea('Aprender UI Design', 'Media');
+let tarea3 = new Tarea('Aprender React.js', 'Alta');
+let tarea4 = new Tarea('Aprender Vue.js', 'Baja');
+
+console.log(tarea1.mostrar());
+console.log(tarea2);
+console.log(tarea3);
