fbd59c9660e49b7b1887afbfd21b1d8aaffc60c7
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 20:36:16 -0700

    Programación Orientada a Objetos  - Herencia

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,4 +1,4 @@
-// Programación Orientada a Objetos - Clases
+// Programación Orientada a Objetos - Clases - Herencia
 
 class Tarea {
     constructor(nombre, prioridad) {
@@ -13,6 +13,18 @@ class Tarea {
     }
 }
 
+class ComprasPendientes extends Tarea {
+    constructor(nombre, prioridad, cantidad) {
+        super(nombre, prioridad);
+        this.cantidad = cantidad;
+    }
+
+    // Reescribir el metodo de la clase Tareas
+    mostrar() {
+        return 'Hola!' + this.nombre;
+    }
+}
+
 // Crear objetos
 let tarea1 = new Tarea('Aprender Javascript', 'Alta');
 let tarea2 = new Tarea('Aprender UI Design', 'Media');
@@ -22,3 +34,11 @@ let tarea4 = new Tarea('Aprender Vue.js', 'Baja');
 console.log(tarea1.mostrar());
 console.log(tarea2);
 console.log(tarea3);
+
+// Compras
+let compra1 = new ComprasPendientes('Jabón', 'Mediana', 4);
+console.log(compra1); // Object { nombre: "Jabón", prioridad: "Mediana", cantidad: 4 }
+console.log(compra1.mostrar()); // Jabón tiene una prioridad de Mediana
+
+// Con metodo mostrar() reescrita
+console.log(compra1.mostrar()); // Hola! Jabón
