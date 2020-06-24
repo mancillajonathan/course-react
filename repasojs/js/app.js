e77e320390f336f386a52d5e6326bf6bf2cc927f
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 17:07:51 -0700

    Métodos para arreglos (.filter .find y .reduce)

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,22 +1,44 @@
-// Spread opertor...
+// Métodos en arreglos
+ 
+const personas = [
+    {nombre: 'Juan',   edad: 22, aprendiedno: 'Javascript'},
+    {nombre: 'Pablo',  edad: 23, aprendiedno: 'PHP'},
+    {nombre: 'Carlos', edad: 21, aprendiedno: 'Adobe XD'},
+    {nombre: 'Miguel', edad: 24, aprendiedno: 'React.js'},
+    {nombre: 'Oscar',  edad: 25, aprendiedno: 'Python'}
+]
+console.log(personas); // Array(5) [ {…}, {…}, {…}, {…}, {…} ]
 
-let lenguajes = ['Javascript', 'PHP', 'Python'];
-let frameworks = ['React.js', 'Vue.js', 'Djando'];
 
-// Unir los arreglos en 1 solo
+// Mayores de 28 años
 
-// Vieja
-let combinacion = lenguajes.concat(frameworks);
-console.log(combinacion);
+const mayores = personas.filter(persona => {
+    return persona.edad > 22;
+})
+console.log(mayores); // Array(3) [ {…}, {…}, {…} ]
 
-// Nueva spread operator
-let nuevoArreglo = [...lenguajes, ...frameworks];
-console.log(nuevoArreglo);
 
-function suma(a, b, c) {
-    console.log(a+b+c);
-}
+personas.filter(persona => {
+    console.log(persona);
+})
 
-const numeros = [10, 10, 10];
+console.log(personas);
+// Object { nombre: "Juan", edad: 22, aprendiedno: "Javascript" }
+// .... Retorna cada objeto individualmente
 
-suma(...numeros);
+
+// Qué aprende Miguel
+const miguel = personas.find(persona => {
+    return persona.nombre === 'Miguel';
+});
+console.log(miguel); // Object { nombre: "Miguel", edad: 24, aprendiedno: "React.js" }
+console.log('Miguel está aprendiendo ' + miguel.aprendiedno); // Miguel está aprendiendo React.js
+
+
+// Suma de todas las edades de personas
+let total = personas.reduce((edadTotal, persona) => {
+    return edadTotal + persona.edad;
+}, 0);
+console.log(total); // 115 toal edad
+console.log(personas.length); // 5 personas
+console.log(total/personas.length); // 23 promedio edad
