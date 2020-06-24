f394e8bbec43f3a6b8723d01e597f67a7d57ddc2
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-23 21:09:10 -0700

    Exportando funciones en módulos

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -1,9 +1,8 @@
 // Exportando funciones en módulos
-import {nombreTarea, crearTarea, tareaCompletada} from './tareas.js';
+import Tarea from './tareas.js';
 
-console.log(nombreTarea);
+const tarea1 = new Tarea('Aprender React.js', 'Inmediata');
 
-const tarea1 = crearTarea('Torres Project', 'Altísima');
 console.log(tarea1);
 
-tareaCompletada();
+tarea1.mostrar();
--- repasojs/js/tareas.js
+++ repasojs/js/tareas.js
@@ -1,16 +1,6 @@
-// Exportar funciones en módulos
-export const nombreTarea = 'Aprender API REST';
+// Exportar clases en módulos
 
-export const crearTarea = (tarea, urgencia) => {
-    return `La tarea ${tarea} tiene una urgencia ${urgencia}`;
-}
-
-export const tareaCompletada = () => {
-    console.log('La tarea se completó');
-}
-
-
-class Tarea {
+export default class Tarea {
     constructor(nombre, prioridad) {
         this.nombre = nombre;
         this.prioridad = prioridad;
@@ -18,19 +8,7 @@ class Tarea {
 
     // Método
     mostrar() {
-        // console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
-        return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
-    }
-}
-
-class ComprasPendientes extends Tarea {
-    constructor(nombre, prioridad, cantidad) {
-        super(nombre, prioridad);
-        this.cantidad = cantidad;
-    }
-
-    // Reescribir el metodo de la clase Tareas
-    mostrar() {
-        return 'Hola!' + this.nombre;
+        console.log(`${this.nombre} tiene una prioridad de ${this.prioridad}`);
+        // return `${this.nombre} tiene una prioridad de ${this.prioridad}`;
     }
 }
