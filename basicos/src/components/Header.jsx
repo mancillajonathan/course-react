46fe093d86ea6ee6895472e41c39ded4d55fee63
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-26 15:51:01 -0700

    Creando nuestro primer componente

---
+++ basicos/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';  // Primer paso siempre  importar la librerí de React
+
+function Header() { // Clear una function declaration
+
+    // En esta parte se puede escribir javascript standard
+
+    return( // To lo que este en el return aparecerá en pantalla
+        <h1>My first component MDF</h1> // Elemento html
+    )
+}
+
+export default Header; // Siempre exportar la función
