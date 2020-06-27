bbb31614171ea5b7f494de3c3455a5393f85ac82
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-06-26 18:11:19 -0700

    Primeros pasos con el state react

--- basicos/src/App.js
+++ basicos/src/App.js
@@ -1,8 +1,32 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer.js'
 
 function App() {
+
+  // Crear listado de productos
+  const [productos, guardarProductos] = useState([
+    {
+      id: 1,
+      nombre: 'Camisa ReactJS',
+      precio: 50
+    },
+    {
+      id: 2,
+      nombre: 'Camisa VueJS',
+      precio: 30
+    },
+    {
+      id: 3,
+      nombre: 'Camisa VanillaJS',
+      precio: 10
+    },
+    {
+      id: 4,
+      nombre: 'Camisa AngularJS',
+      precio: 150
+    }
+  ]);
 
   // Obtener la fecha
   const fecha = new Date().getFullYear();  
