9c3f5140be5f24ad93537b732d50418bcf4004a5
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-09 15:00:13 -0700

    Mostrar un mensaje de forma condicional

--- proyectos/citas/src/App.js
+++ proyectos/citas/src/App.js
@@ -17,6 +17,9 @@ function App() {
     setCitas(nuevasCitas);
   };
 
+  // Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
@@ -27,7 +30,7 @@ function App() {
             <Formulario crearCita={crearCita} />
           </div>
           <div className='one-half column'>
-            <h2>Administra tus citas</h2>
+            <h2>{titulo}</h2>
             {citas.map((cita) => (
               <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
             ))}
