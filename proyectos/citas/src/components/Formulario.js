8e23ad47b55c9b3e3ad1308e954d308c9e3bbc92
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-08 23:01:50 -0700

    Agregando los componentes al formulario

--- proyectos/citas/src/components/Formulario.js
+++ proyectos/citas/src/components/Formulario.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Formulario = () => {
-  return <h1>Desde Formulario</h1>;
+  return (
+    <Fragment>
+      <h2>Crear cita</h2>
+
+      <form action=''>
+        <label htmlFor=''>Nombre mascota</label>
+        <input
+          type='text'
+          name='mascota'
+          className='u-full-width'
+          placeholder='Nombre mascota'
+        />
+
+        <label htmlFor=''>Nombre dueño</label>
+        <input
+          type='text'
+          name='propietario'
+          className='u-full-width'
+          placeholder='Nombre dueño de la mascota'
+        />
+
+        <label htmlFor=''>Fecha</label>
+        <input type='date' name='fecha' className='u-full-width' />
+
+        <label htmlFor=''>Hora</label>
+        <input type='time' name='hora' className='u-full-width' />
+      </form>
+      <label htmlFor=''>Síntomas</label>
+      <textarea className='u-full-width' name='sinstomas'></textarea>
+      <button
+        type='submit'
+        className='u-full-width button-primary  '
+      >
+          Agregar cita
+      </button>
+    </Fragment>
+  );
 };
 
 export default Formulario;
