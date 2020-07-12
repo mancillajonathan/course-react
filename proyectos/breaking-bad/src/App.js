437f42de5017cf83e7f5754f0b5ccea5a54e4a8a
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-12 16:13:02 -0700

    Agregando emotion

--- proyectos/breaking-bad/src/App.js
+++ proyectos/breaking-bad/src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #ffff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border 2px solid black;
+`;
 
 function App() {
+  const consultarAPI = () => {
+    console.log('Consultando...');
+  };
+
   return (
-    <h1>Braking Bad</h1>
+    <Container>
+      <Button onClick={consultarAPI}>Obtener frase</Button>
+    </Container>
   );
 }
 
