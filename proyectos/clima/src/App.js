36dd72ab60507de8f1b8ec615d63426d0b0ad5ce
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-12 21:00:27 -0700

    Creando el formulario

--- proyectos/clima/src/App.js
+++ proyectos/clima/src/App.js
@@ -1,10 +1,20 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <Fragment>
       <Header titulo='Clima React App' />
+
+      <div className='contenedor-form'>
+        <div className='row'>
+          <div className='col m6 s12'>
+            <Formulario />
+          </div>
+          <div className='col m6 s12'>2</div>
+        </div>
+      </div>
     </Fragment>
   );
 }
