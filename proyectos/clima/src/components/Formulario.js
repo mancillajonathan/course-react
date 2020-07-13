ae5fe370ddc1929403c173a0b25679a2a1e4ef1b
Jonathan Mancilla <jairmancillaportillo@gmail.com>
2020-07-12 23:36:14 -0700

    Creando el State y leyendo lo que el usuario selec

--- proyectos/clima/src/components/Formulario.js
+++ proyectos/clima/src/components/Formulario.js
@@ -1,16 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+  // State del formulario
+  const [busqueda, guardasBusqueda] = useState({
+    ciudad: '',
+    pais: '',
+  });
+
+  // extraer ciudad y pais
+  const { ciudad, pais } = busqueda;
+
+  // Funciones que coloca los elementos en el state
+  const handleChange = (event) => {
+    //actualizar el statte
+    guardasBusqueda({
+      ...busqueda,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div>
       <form action=''>
         <div className='input-field col s12'>
-          <input type='text' name='ciudad' id='ciudad' />
+          <input
+            type='text'
+            name='ciudad'
+            id='ciudad'
+            value={ciudad}
+            onChange={handleChange}
+          />
           <label htmlFor='ciudad'>Ciudad: </label>
         </div>
         <div className='input-field col s12'>
-          <select name='pais' id='pais'>
+          <select
+            name='pais'
+            id='pais'
+            value={pais}
+            onChange={handleChange}
+            // onScrol={handleScrool}
+          >
             <option value=''>--- Seleccione un país ---</option>
+            <option value='US'>Estados Unidos</option>
+            <option value='MX'>México</option>
+            <option value='AR'>Argentina</option>
+            <option value='CO'>Colombia</option>
+            <option value='CR'>Costa Rica</option>
+            <option value='ES'>España</option>
+            <option value='PE'>Perú</option>
           </select>
           <label htmlFor='pais'>País: </label>
         </div>
